3cb7073e78af7ff402839f32d376e70838a48134
NightDreams <yayodcv@gmail.com>
2022-04-13 23:06:16 -0500

    c4 - efectos con useEffect

--- src/UseState.js
+++ src/UseState.js
@@ -1,17 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 export function UseState({ name }) {
   const [error, setError] = useState(true);
+  const [loading, setLoading] = useState(false);
+
+  useEffect(() => {
+    console.log("Empezando el efecto");
+    if (!!loading) {
+      setTimeout(() => {
+        console.log("Haciendo la validación");
+        setLoading(false);
+        console.log("Terminando la validación");
+      }, 3000);
+    }
+    console.log("Terminando el efecto");
+  }, [loading]);
 
   return (
     <div>
       <h2>Eliminar {name}</h2>
       <p>por favor, escribe el codigo de seguridad </p>
       {error && <p> Error: el código es incorrecto</p>}
+      {loading && <p>Cargando... </p>}
       <input type="text" placeholder="Código de seguridad" />
-      <button onClick={() => setError((prevState) => !prevState)}>
-        Comprobar
-      </button>
+      <button onClick={() => setLoading(true)}>Comprobar</button>
     </div>
   );
 }
