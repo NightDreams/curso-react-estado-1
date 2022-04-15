ccc3c50a810015ebeb1322bc986ce327e91ba9ad
NightDreams <yayodcv@gmail.com>
2022-04-15 12:01:24 -0500

    c8 - compound states with react.component, check SEGURITY_CODE.

--- src/ClasState.js
+++ src/ClasState.js
@@ -1,12 +1,16 @@
 import React, { Component } from "react";
 import { Loading } from "./Loading";
+
+const SEGURITY_CODE = "paradigma";
+
 export class ClasState extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      error: true,
+      error: false,
       loading: false,
+      value: ""
     };
   }
 
@@ -23,7 +27,15 @@ export class ClasState extends Component {
     if (!!this.state.loading) {
       setTimeout(() => {
         console.log("Haciendo la validación");
+        if (SEGURITY_CODE === this.state.value) {
+
+          this.setState({ error: false });
+        } else {
+
+          this.setState({ error: true });
+        }
         this.setState({ loading: false });
+
         console.log("Terminando la validación");
       }, 3000);
     }
@@ -34,9 +46,21 @@ export class ClasState extends Component {
       <div>
         <h2>Eliminar {this.props.name}</h2>
         <p>por favor, escribe el codigo de seguridad. </p>
-        {this.state.error && <p> Error: el código es incorrecto</p>}
+        {(this.state.error && !this.state.loading) &&
+          <p> Error: el código es incorrecto</p>
+        }
         {this.state.loading && <Loading />}
-        <input type="text" placeholder="Código de seguridad" />
+
+        <input
+          type="text"
+          placeholder="Código de seguridad"
+          value={this.state.value}
+          onChange={(event) => {
+            this.setState({
+              value: event.target.value
+            });
+
+          }} />
         <button onClick={() => this.setState({ loading: true })}>
           Comprobar
         </button>
