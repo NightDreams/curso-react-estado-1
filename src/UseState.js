7a07adb368c131d6277b0ea144a99735d004db0b
NightDreams <yayodcv@gmail.com>
2022-04-14 19:52:54 -0500

    c6 Setup dinamic state , and comprobe segurity code.  independent states with useState

--- src/UseState.js
+++ src/UseState.js
@@ -1,14 +1,24 @@
 import React, { useState, useEffect } from "react";
 
-export function UseState({ name }) {
-  const [error, setError] = useState(true);
+const SEGURITY_CODE = "paradigma";
+
+export const UseState = ({ name }) => {
+  const [value, setValue] = useState("");
+  const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
+
+  console.log(value);
 
   useEffect(() => {
     console.log("Empezando el efecto");
     if (!!loading) {
+      // emulate backend response
       setTimeout(() => {
         console.log("Haciendo la validación");
+        //check security code
+        if (value !== SEGURITY_CODE) {
+          setError(true);
+        }
         setLoading(false);
         console.log("Terminando la validación");
       }, 3000);
@@ -22,8 +32,23 @@ export function UseState({ name }) {
       <p>por favor, escribe el codigo de seguridad </p>
       {error && <p> Error: el código es incorrecto</p>}
       {loading && <p>Cargando... </p>}
-      <input type="text" placeholder="Código de seguridad" />
-      <button onClick={() => setLoading(true)}>Comprobar</button>
+
+      <input
+        type="text"
+        placeholder="Código de seguridad"
+        onChange={({ target: { value } }) => {
+          setValue(value);
+          setError(false);
+        }}
+      />
+      <button
+        onClick={() => {
+          setLoading(true);
+          // setError(false);
+        }}
+      >
+        Comprobar
+      </button>
     </div>
   );
-}
+};
