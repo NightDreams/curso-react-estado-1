c75aea6c86bcab38bbb8355a9e76f0c84257c2e7
NightDreams <yayodcv@gmail.com>
2022-04-16 10:55:05 -0500

    c10 - Setup imperative states, conditional render with compound states

--- src/UseState.js
+++ src/UseState.js
@@ -6,7 +6,9 @@ export const UseState = ({ name }) => {
   const [state, setState] = useState({
     value: "",
     error: false,
-    loading: false
+    loading: false,
+    deleted: false,
+    confirmed: false,
   });
 
   console.log(state);
@@ -19,7 +21,7 @@ export const UseState = ({ name }) => {
         console.log("Haciendo la validación");
         //check security code
         if (state.value === SEGURITY_CODE) {
-          setState({ ...state, error: false, loading: false });
+          setState({ ...state, error: false, loading: false, confirmed: true });
         } else {
           setState({ ...state, error: true, loading: false });
         }
@@ -30,30 +32,67 @@ export const UseState = ({ name }) => {
     console.log("Terminando el efecto");
   }, [state.loading]);
 
-  return (
-    <div>
-      <h2>Eliminar {name}</h2>
-      <p>por favor, escribe el codigo de seguridad </p>
-      {(state.error && !state.loading) && (
-        <p> Error: el código es incorrecto</p>
-      )}
-      {state.loading && <p>Cargando... </p>}
+  if (!state.deleted && !state.confirmed) {
+    return (
+      <div>
+        <h2>Eliminar {name}</h2>
+        <p>por favor, escribe el codigo de seguridad </p>
+        {state.error && !state.loading && (
+          <p> Error: el código es incorrecto</p>
+        )}
+        {state.loading && <p>Cargando... </p>}
 
-      <input
-        type="text"
-        placeholder="Código de seguridad"
-        value={state.value}
-        onChange={({ target: { value } }) => {
-          setState({ ...state, value: value });
-        }}
-      />
-      <button
-        onClick={() => {
-          setState({ ...state, loading: true });
-        }}
-      >
-        Comprobar
-      </button>
-    </div >
-  );
+        <input
+          type="text"
+          placeholder="Código de seguridad"
+          value={state.value}
+          onChange={({ target: { value } }) => {
+            setState({ ...state, value: value });
+          }}
+        />
+        <button
+          onClick={() => {
+            setState({ ...state, loading: true });
+          }}
+        >
+          Comprobar
+        </button>
+      </div>
+    );
+  } else if (!!state.confirmed && !state.deleted) {
+    return (
+      <>
+        <p>Pedimos configimacion ¿Estás seguro?</p>
+        <button
+          // Reegresar a la pantalla anterior.
+          onClick={() => {
+            setState({ ...state, deleted: true });
+          }}
+        >
+          Sí, eliminar
+        </button>
+        <button
+          onClick={() => {
+            setState({ ...state, confirmed: false, value: "" });
+          }}
+        >
+          No, me arrepenti
+        </button>
+      </>
+    );
+  } else {
+    return (
+      <>
+        <p>Eliminado con éxito </p>
+        <button
+          // Reegresar a la pantalla anterior.
+          onClick={() => {
+            setState({ ...state, confirmed: false, deleted: false, value: "" });
+          }}
+        >
+          Resetear, Volver atrás
+        </button>
+      </>
+    );
+  }
 };
